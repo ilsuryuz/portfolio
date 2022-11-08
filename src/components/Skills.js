da81e4ae7396bf905b691f7f8ded98056c334232
Ilsur <yuzlikeyevi@gmail.com>
2022-11-08 12:37:55 -0600

    React warnings fixed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,73 +7,73 @@ function Skills() {
             <div className='skills-content'>
                 <ul className='list-skill'>
                     <li>
-                        <img src='technologiesImg/js.png' />
+                        <img src='technologiesImg/js.png' alt='js' />
                         JAVASCRIPT
                     </li>
                     <li>
-                        <img src='technologiesImg/html5.png' />
+                        <img src='technologiesImg/html5.png' alt='html5'/>
                         HTML5
                     </li>
                     <li>
-                        <img src='technologiesImg/css3.png' />
+                        <img src='technologiesImg/css3.png' alt='css3'/>
                         CSS3
                     </li>
                     <li>
-                        <img src='technologiesImg/python.png' />
+                        <img src='technologiesImg/python.png' alt='python'/>
                         PYTHON
                     </li>                             
                 </ul>
                 <ul className='list-skill'>
                     <li>
-                        <img src='technologiesImg/nodejs.png' />
+                        <img src='technologiesImg/nodejs.png' alt='nodejs'/>
                         NODE.JS
                     </li>
                     <li>
-                        <img src='technologiesImg/nodejs.png' />
+                        <img src='technologiesImg/nodejs.png' alt='expressjs'/>
                         EXPRESS.JS
                     </li>
                     <li>
-                        <img src='technologiesImg/nodejs.png' />
+                        <img src='technologiesImg/nodejs.png' alt='mongoose'/>
                         MONGOOSE
                     </li>
                     <li>
-                        <img src='technologiesImg/mongodb.png' />
+                        <img src='technologiesImg/mongodb.png' alt='mongodb'/>
                         MONGODB
                     </li>
                 </ul>
                 <ul className='list-skill'>
                     <li>
-                        <img src='technologiesImg/react.png' />
+                        <img src='technologiesImg/react.png' alt='react'/>
                         REACT
                     </li>
                     <li>
-                        <img src='technologiesImg/django.svg' />
+                        <img src='technologiesImg/django.svg' alt='django'/>
                         DJANGO
                     </li>
                     <li>
-                        <img src='technologiesImg/socketio.svg' />
+                        <img src='technologiesImg/socketio.svg' alt='socketio'/>
                         SOCKET.IO
                     </li>
                     <li>
-                        <img src='technologiesImg/jquery.png' />
+                        <img src='technologiesImg/jquery.png' alt='jquery'/>
                         JQUERY
                     </li>
                 </ul>
                 <ul className='list-skill'>
                     <li>
-                        <img src='technologiesImg/psql.png' />
+                        <img src='technologiesImg/psql.png' alt='postgresql'/>
                         POSTGRESQL
                     </li>
                     <li>
-                        <img src='technologiesImg/firebase.png' />
+                        <img src='technologiesImg/firebase.png' alt='firebase'/>
                         FIREBASE
                     </li>
                     <li>
-                        <img src='technologiesImg/git.png' />
+                        <img src='technologiesImg/git.png' alt='git'/>
                         GIT
                     </li>
                     <li>
-                        <img src='technologiesImg/materialize.png' />
+                        <img src='technologiesImg/materialize.png' alt='materialize'/>
                         MATERIALIZE
                     </li>
                 </ul>
